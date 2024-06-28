d67b39e11cd0c2298804c8d0bfb64bae415fe65c
Serhan Altug <saltug@yahoo.com>
2024-06-28 14:01:15 +0300

    User entity updated

--- PERforMAX.Domain/Entities/User.cs
+++ PERforMAX.Domain/Entities/User.cs
@@ -6,6 +6,7 @@
         public string Lastname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
 
     }
 }
