b2f798d1c459fc0e818621850d4868102dc228eb
Serhan Altug <saltug@yahoo.com>
2024-06-28 14:02:51 +0300

    .

--- PERforMAX.Domain/Entities/User.cs
+++ PERforMAX.Domain/Entities/User.cs
@@ -6,6 +6,8 @@
         public string Lastname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+
 
     }
 }
